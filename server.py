5b2448a0694cf4de7b77e00ebf39ac42ccd7a72e
KimInteger <kjs20151240@gmail.com>
2024-06-28 20:23:09 +0900

    docs_김정수 : 오타수정

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-print('hello server');
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+host = 'localhost'
+port = 8000
+
+server = HTTPServer((host, port), SimpleHTTPRequestHandler)
+print(f"server started at http://{host}:{port}")
+
+server.serve_forever()
